0ac5dec1baa217c895f0084a344b00eaa59c0f3a
Jv Kyle Eclarin <mathemusician@users.noreply.github.com>
2021-10-24 19:49:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ import streamlit as st
 from transformers import Wav2Vec2Processor
 from pathlib import Path
 from copy import deepcopy
-from consolidate import time_decoder
-from split_texts import split_by_words
-from make_xml import make_xml_from_words
 from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
 from flash.audio import SpeechRecognition, SpeechRecognitionData
 
