d4ef97cfc7d3ea8ec919d29d90ec7926f0b0fedf
Jv Kyle Eclarin <mathemusician@users.noreply.github.com>
2021-10-24 22:42:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ st.title("Audio Transcription")
 uploaded_file = st.file_uploader("Choose an audio file")
 
 if uploaded_file is not None:
-  a = uploaded_file.read()
+  a = soundfile.read(io.BytesIO(uploaded_file.read())
   b = deepcopy(a)
   # b = bytes(str(b), "utf-8")
   st.write(a == b)
