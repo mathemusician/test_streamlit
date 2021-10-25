9e0ee75ca411cd6f6f484a678ad6eae1fe2bc462
Jv Kyle Eclarin <mathemusician@users.noreply.github.com>
2021-10-24 22:56:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
 from flash.audio import SpeechRecognition, SpeechRecognitionData
 import io
 import scipy.signal as sps
+from scipy.io import wavfile
+
 
 st.title("Audio Transcription")
 
@@ -19,7 +21,7 @@ st.title("Audio Transcription")
 uploaded_file = st.file_uploader("Choose an audio file")
 
 if uploaded_file is not None:
-  sample_rate, clip = soundfile.read(io.BytesIO(uploaded_file.read()))
+  sample_rate, clip = wavfile.read(io.BytesIO(uploaded_file.read()))
   number_of_samples = round(len(clip) * float(new_rate) / sample_rate)
   clip = sps.resample(clip, number_of_samples)
   # b = bytes(str(b), "utf-8")
