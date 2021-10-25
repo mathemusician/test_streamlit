97f115fa776f535ef0d6df8b65399103f8476655
Jv Kyle Eclarin <mathemusician@users.noreply.github.com>
2021-10-24 22:47:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,5 +20,6 @@ uploaded_file = st.file_uploader("Choose an audio file")
 if uploaded_file is not None:
   sr, x = soundfile.read(io.BytesIO(uploaded_file.read()))
   # b = bytes(str(b), "utf-8")
+  resampled_audio = librosa.resample(x, sr, 16000)
   st.write(sr, x)
   # file_bytes = np.asarray(bytearray(uploaded_file.read()))
