1d7a9689b1d1bb507a92a555f0494e31fbbc3310
Jv Kyle Eclarin <mathemusician@users.noreply.github.com>
2021-11-08 16:42:33 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import pandas as pd
 from moviepy.editor import VideoFileClip
 import streamlit as st
 
+i = 0
 def pipeline(frame):
+    i += 1
     try:
         cv2.putText(frame, str(next(dfi)[1].item()), (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 3, cv2.LINE_AA, True)
     except StopIteration:
@@ -18,5 +20,6 @@ video = VideoFileClip("color.mp4")
 out_video = video.fl_image(pipeline)
 out_video.write_videofile("color2.mp4", codec="libx264")
 
+st.write(i)
 
 st.video("color2.mp4")
