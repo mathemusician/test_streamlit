bc279e52dd56efc3175c360ece65ed6498f4efc5
Jv Kyle Eclarin <mathemusician@users.noreply.github.com>
2021-11-08 16:11:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import streamlit as st
-from moviepy.editor import ColorClip
+import gdown
+from moviepy.editor import ColorClip, TextClip
+from moviepy.video.tools.subtitles import SubtitlesClip
 
 st.write("Video Creation")
 
-result = ColorClip(size=(200, 100), 
-		   color=(0,0,0), 
-		   duration=5).write_videofile('color.mp4', fps=24)
+if "Success4.mov" not in list_of_vids:
+        url = "https://drive.google.com/uc?id=1kUO0dKTsq4E2rFH1_JehUZC23giwVtY3"
+        output = "Success4.mov"
+        gdown.download(url, output, quiet=False)
+    
+generator = lambda txt: TextClip(txt, font='Arial', fontsize=24, color='white')
+subs = [((0, 4), 'subs1'),
+        ((4, 9), 'subs2'),
+        ((9, 12), 'subs3'),
+        ((12, 16), 'subs4')]
 
-st.video('color.mp4')
+subtitles = SubtitlesClip(subs, generator)
+
+video = VideoFileClip("input.mp4")
+result = CompositeVideoClip([video, subtitles.set_pos(('center','bottom'))])
+
+result.write_videofile("output.mp4", fps=video.fps, temp_audiofile="temp-audio.m4a", remove_temp=True, codec="libx264", audio_codec="aac")
+
+
+st.video('output.mp4')
